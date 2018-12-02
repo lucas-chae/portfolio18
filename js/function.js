e1bbb193e25bd17dfd453fd1dc3b48705f7b1b30
Lucas Chae <chae1240@hotmail.com>
2018-12-02 19:22:57 +0900

    mobile working

--- js/function.js
+++ js/function.js
@@ -1,11 +1,14 @@
 $(function() {
+    if ($(window).width() >= 768) {
         var elem = $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
             {
            // height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
             onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
                 console.log(percent);
             }
+
         });
+      }
         $(window).resize(function() {
           if ($(window).width() <= 768) {
             elem.destroy();
@@ -31,6 +34,47 @@ $(function() {
       });
    });
 
+
+
+   $(function() {
+    $(window).scroll(function () {
+
+      if ($(window).width() <= 768) {
+      if ($(this).scrollTop() >50  ) {
+       $("body").addClass("change-color");
+       $("nav").addClass("change-color-nav");
+       
+      }
+      else {
+       $("body").removeClass("change-color");
+       $("nav").removeClass("change-color-nav");
+       
+      }
+    }
+
+      if ($(window).width() <= 768) {
+
+      if ($(this).scrollTop() > 150) {
+        $("#title").addClass("displayUp");
+      }
+      else {
+        $("#title").removeClass("displayUp");
+      }
+    }
+
+
+
+
+   });
+    $(window).resize(function() {
+      if ($(window).width() >= 768) {
+        elem.destroy();
+      }
+      else {
+        elem.reinitialize();
+      }
+    });
+});
 
 /*
 $(function() {
