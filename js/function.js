5c8339acb3baa368818db89795ae70b32951c188
Lucas Chae <chae1240@hotmail.com>
2018-12-05 09:43:51 +0900

    background fix

--- js/function.js
+++ js/function.js
@@ -2,9 +2,10 @@ $(function() {
     if ($(window).width() >= 768) {
         var elem = $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
             {
-           // height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
+           //height: 10050, 
+                            // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
             onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
-                console.log(percent);
+               
             }
 
         });
@@ -34,6 +35,30 @@ $(function() {
       });
    });
 
+   $(function() {
+    $(window).scroll(function () {
+     
+       var menuTrigger = $("#scroll-container").offset();
+       var viewWidth = $(window).width();
+       console.log("menuTrigger is" + menuTrigger.left);
+       console.log("viewWidth is" + viewWidth);
+       if (menuTrigger.left <= -0.8 * viewWidth) {
+        $("#list-portfolio").addClass("effective");
+        $("#list-about").addClass("ineffective");
+        $("#list-contact").addClass("ineffective");
+        $("#design").removeClass("displayNone");
+       }
+       else {
+        $("#list-portfolio").removeClass("effective");
+        $("#list-about").removeClass("ineffective");
+        $("#list-contact").removeClass("ineffective");
+        $("#design").addClass("displayNone");
+       }
+
+       
+    });
+ });
+
 
 
    $(function() {
@@ -43,7 +68,6 @@ $(function() {
       if ($(this).scrollTop() >50  ) {
        $("body").addClass("change-color");
        $("nav").addClass("change-color-nav");
-       
       }
       else {
        $("body").removeClass("change-color");
@@ -62,9 +86,6 @@ $(function() {
       }
     }
 
-
-
-
    });
     $(window).resize(function() {
       if ($(window).width() >= 768) {
@@ -75,6 +96,7 @@ $(function() {
       }
     });
 });
+
 
 /*
 $(function() {
